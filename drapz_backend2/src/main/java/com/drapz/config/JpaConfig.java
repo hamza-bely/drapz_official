9ca37e2e06aa987d359099e1a7082d0f9182c660
hamza-bely <hamzaciao80@gmail.com>
2025-11-17 20:39:08 +0100

    delete file

--- drapz_backend2/src/main/java/com/drapz/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.drapz.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "com.drapz.repository")
-public class JpaConfig {
-}
