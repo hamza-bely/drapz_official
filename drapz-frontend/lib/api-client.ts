8a8aab029c29ae504c07cf94b7a404bd2cbd8c44
hamza.belyahiaoui <hamza.belyahiaoui@spie.com>
2025-12-15 15:59:05 +0100

    Remove Cesium library and configure API URL from .env

--- drapz-frontend/lib/api-client.ts
+++ drapz-frontend/lib/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //ne pas change
-const API_BASE_URL = 'http://localhost:8080/api/api/';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
